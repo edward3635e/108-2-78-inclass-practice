305b89da3a74af33ee35b8a411b724ffe5898e3a
edward3635e <61775730+edward3635e@users.noreply.github.com>
2020-04-04 20:19:54 +0800

    04042020 practice
    Practice

---
+++ 04042020.R
@@ -0,0 +1,14 @@
+```{r}
+vectorExample <- c(2,6,7)
+listExample <- list(2,6,7)
+
+print(vectorExample)
+print(listExample)
+
+```{r}
+c("2020-03-31T13:40:55Z","AA","2","BB")
+
+Library(lubridate)
+`CCC` <- list(
+  ymd_hms("2020-03-31T13:40:55Z"), #Date/Time "AA", #Character2, Numeric "BB" #Character)
+     
