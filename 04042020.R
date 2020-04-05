0dac03dd11ad684e40e8bdd134c06efc6c422835
edward3635e <61775730+edward3635e@users.noreply.github.com>
2020-04-05 14:37:01 +0800

    Update 04042020.R

--- 04042020.R
+++ 04042020.R
@@ -1,14 +1,28 @@
+title: "List"
+author: "Edward"
+Date:"03/31/2020"
+output:html_document
+
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+##list
+
 ```{r}
 vectorExample <- c(2,6,7)
 listExample <- list(2,6,7)
 
 print(vectorExample)
 print(listExample)
+````
 
 ```{r}
 c("2020-03-31T13:40:55Z","AA","2","BB")
 
 Library(lubridate)
-`CCC` <- list(
-  ymd_hms("2020-03-31T13:40:55Z"), #Date/Time "AA", #Character2, Numeric "BB" #Character)
+`CCC` <- list
+  (ymd_hms("2020-03-31T13:40:55Z"), #Date/Time "AA", #Character2, Numeric "BB" #Character )
      
